d5e6e7559e9e264176d5fedbb681e40754cc3429
Szymon <edbsiedlce@gmail.com>
2024-03-10 08:19:03 +0100

    Styled in SCSS-divided in Grid

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <main className='main'>
+          <h1 className='header'>header header header headerheader header header header</h1>
+          <aside className='personal'>Dane osobowe
+              <img className='photo' src="szymon.jpg" alt=""/>
+          <ul>
+              <li></li>
+          </ul></aside>
+          <section className='details'>
+              <h2>Doświadczenie</h2>
+              <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+              </ul>
+              <h2>Wykształcenie</h2>
+              <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+              </ul>
+          </section>
+          
+      </main>
+   
+      
   </React.StrictMode>,
 )
